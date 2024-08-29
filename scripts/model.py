9f2cf38fc7d86e45e18d5f253ef415b66e659e6c
Zeynep Behşi <behsi2008@gmail.com>
2024-08-30 00:58:57 +0300

    "Rename and reorganize files"

--- scripts/model.py
+++ scripts/model.py
@@ -29,18 +29,3 @@ def create_vgg16_model(input_shape):
                   metrics=['accuracy'])
     
     return model
-
-def train_model(model, train_generator, val_generator, epochs):
-    # Stop training when a monitored quantity has stopped improving
-    early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
-
-    # Reduce learning rate when a metric has stopped improving
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.00001)
-
-    # Train the model with the specified callbacks
-    history = model.fit(train_generator, 
-                        validation_data=val_generator, 
-                        epochs=epochs, 
-                        callbacks=[early_stopping, reduce_lr])
-    
-    return history
